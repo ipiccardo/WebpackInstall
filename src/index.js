dc4a7ca5d6db9c68a116c0ad58a8a9fd0fda3858
ipiccard <ivan.piccardo@tsoftglobal.com>
2022-07-15 22:36:55 -0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
-import { Todo, TodoList } from "./classes/index";
-import { crearTodoHtml } from "./js/componentes";
+import { saludar } from './js/componentes.js';
+import './styles.css'
 
-import "./styles.css";
+const nombre = 'Ivan';
 
-export const todoList = new TodoList();
-
-todoList.todos.forEach((todo) => crearTodoHtml(todo));
-
-const newTodo = new Todo("Aprender JavaScript");
-todoList.nuevoTodo(newTodo);
-
-console.log("todos", todoList.todos);
-
-const tarea = new Todo("Aprender JavaScript!!!");
-
-tarea.completado = true;
-
-todoList.nuevoTodo(tarea);
-
-console.log(todoList);
-
-crearTodoHtml(tarea);
-
-localStorage.setItem("mi-key", "ABC123");
+saludar( nombre );
